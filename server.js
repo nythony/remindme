14cca1864b347bd7a1e048b9fbb1a53f4026b2f6
Alina Chaiyasarikul <alina@bu.edu>
2019-11-03 06:08:00 -0500

    printing out req packet to try to get num

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ app.get('/login',(req,res)=>{
 
 
 app.post('/sms', function(req, res) {
-  console.log("req is" + req.query.from);
+  console.log("req is" + req.body.from);
 
   var promise1 = new Promise(function(resolve, reject) {
     var twilio = require('twilio');
