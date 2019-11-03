65aa3969adeeb1c2eeba37e72a0612df0cd478b8
Alina Chaiyasarikul <alina@bu.edu>
2019-11-03 10:44:10 -0500

    Register password

--- server.js
+++ server.js
@@ -317,7 +317,11 @@ app.post('/regpass', function(req, res) {
               var dbnum = row["phonenum"];
               var dbpass = row["pass"] 
 
-              if (dbnum == num && typeof dbpass == undefined) {
+              console.log(dbnum + ' and ' + dbpass);
+              console.log(dbnum == num);
+              console.log(tyepof dbpass == undefined);
+
+              if (dbnum == num && typeof dbpass == "undefined") {
                 console.log("true");
 
                 client.query('UPDATE RegNum SET pass =  \''+pass+'\' WHERE phonenum =  \''+ num+'\';', (err, res) => {
